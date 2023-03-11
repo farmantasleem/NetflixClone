5d46fbf0627b280e5f398c6f20145decbf24d864
unknown <iamfarmanmalik@gmail.com>
2023-03-11 15:30:27 +0530

    minor update

--- server/model/ListModel.js
+++ server/model/ListModel.js
@@ -1,8 +1,8 @@
 const mongoose=require("mongoose");
 
 const MylistSchema=mongoose.Schema({
-        user:{type:mongoose.Types.ObjectId,required:true},
-        movie:{type:mongoose.Types.ObjectId,required:true}
+        user:{type:mongoose.Types.ObjectId,required:true,ref:'user'},
+        movie:{type:mongoose.Types.ObjectId,required:true,ref:'movie'}
 })
 
 const MylistModel=mongoose.model("mylist",MylistSchema);
