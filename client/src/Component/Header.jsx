30a3530ddd5b7bc47021f2add5b962eab3cd27d8
unknown <iamfarmanmalik@gmail.com>
2023-03-12 22:42:34 +0530

    added responsiveness to homepage

--- client/src/Component/Header.jsx
+++ client/src/Component/Header.jsx
@@ -5,8 +5,8 @@ import logo from "../assests/logo.png"
 export const Header=()=>{
     return (
         <Stack p="20px" direction={"row"}  justifyContent="space-between" minW="100%" >
-            <Image width="200px" src={logo}/>            
-        <NavLink to={"/user"}>   <Button height={"40px"} fontSize="19px" width="160px" bgColor="rgb(229,9,20)" color={"white"}>Sign In</Button></NavLink>
+            <Image width={{base:"100px",md:"200px" }}src={logo}/>            
+        <NavLink to={"/user"}>   <Button height={{base:"40px",md:"40px"}} fontSize={{base:"17px",md:"19px"}} width="160px" bgColor="rgb(229,9,20)" color={"white"}>Sign In</Button></NavLink>
         </Stack>
     )
 }
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -36,7 +36,7 @@ export const Home = () => {
   },[auth])
 
   return (
-    <Container minW="100%" p="0%">
+    <Container minW="100%" maxW={"100%"} p="0%" overflow={"hidden"}>
       <Container
         minW="100%"
         minH="820px"
@@ -52,16 +52,16 @@ export const Home = () => {
           pb="320px"
           pt="320px"
         >
-          <Stack gap="5px" justifyContent={"center"} textAlign="center">
-            <Heading as={"h2"} fontSize="55px" maxW={"590px"}>
+          <Stack gap="5px"  justifyContent={"center"} textAlign="center">
+            <Heading as={"h2"} fontSize={{base:"40px",md:"55px"}} maxW={"590px"}>
               Unlimited movies, TV shows and more.
             </Heading>
-            <Text fontSize={"25px"}>Watch anywhere. Cancel anytime.</Text>
-            <Text fontWeight={"600"} fontSize={"20px"}>
+            <Text  fontSize={{base:"17px",md:"20px"}}>Watch anywhere. Cancel anytime.</Text>
+            <Text fontWeight={"600"} fontSize={{base:"17px",md:"20px"}}>
               Ready to watch? Enter your email to create or restart your
               membership.
             </Text>
-            <Stack direction={"row"}>
+            <Stack direction={{base:"column",md:"row"}}>
               <Input
                 bgColor={"white"}
                 color="black"
@@ -73,9 +73,10 @@ export const Home = () => {
                 bgColor="rgb(244,6,18)"
                 height="50px"
                 _hover={{}}
-                fontSize="22px"
+                fontSize={{base:"18px",md:"22px"}}
                 pl="50px"
                 pr="50px"
+                minW={{base:"100%",md:"unset"}}
               >
                 Get Started
               </Button></NavLink>
@@ -86,10 +87,10 @@ export const Home = () => {
 
       <Container  borderBottom="8px"
         borderBottomColor={"rgb(34,34,34)"} p="40px" minW="100%" justifyContent={"center"} justifyItems="center" bgColor="rgb(0,0,0)">
-      <Stack color="rgb(178,178,178)" direction="row" alignItems={"center"} justifySelf={"center"} m="auto" maxW="80%">
+      <Stack color="rgb(178,178,178)" gap={{base:"30px",md:"0px"}}  direction={{base:"column",md:"row"}} alignItems={"center"} justifySelf={"center"} m="auto" maxW="80%">
         <Stack direction={"column"}>
-          <Heading fontSize="55px">Enjoy on your TV.</Heading>
-          <Text fontSize={"30px"}>
+          <Heading fontSize={{base:"35px",md:"55px"}}>Enjoy on your TV.</Heading>
+          <Text  fontSize={{base:"20px",md:"30px"}}>
             Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray
             players and more.
           </Text>
@@ -100,11 +101,11 @@ export const Home = () => {
 
       <Container  borderBottom="8px"
         borderBottomColor={"rgb(34,34,34)"} p="40px" minW="100%" justifyContent={"center"} justifyItems="center" bgColor="rgb(0,0,0)">
-      <Stack color="rgb(178,178,178)" direction="row-reverse" alignItems={"center"} justifySelf={"center"} m="auto" maxW="80%">
+      <Stack color="rgb(178,178,178)" gap={{base:"30px",md:"0px"}}  direction={{base:"column",md:"row-reverse"}} alignItems={"center"} justifySelf={"center"} m="auto" maxW="80%">
         <Stack direction={"column"}>
-          <Heading fontSize="55px">Download your shows to watch offline.
+          <Heading fontSize={{base:"35px",md:"55px"}}>Download your shows to watch offline.
 </Heading>
-          <Text fontSize={"30px"}>
+          <Text fontSize={{base:"20px",md:"30px"}}>
           Save your favourites easily and always have something to watch.
 
           </Text>
@@ -115,12 +116,12 @@ export const Home = () => {
 
       <Container  borderBottom="8px"
         borderBottomColor={"rgb(34,34,34)"} p="40px" minW="100%" justifyContent={"center"} justifyItems="center" bgColor="rgb(0,0,0)">
-      <Stack color="rgb(178,178,178)" direction="row" alignItems={"center"} justifySelf={"center"} m="auto" maxW="80%">
-        <Stack direction={"column"}>
-          <Heading fontSize="55px">Watch everywhere.
+      <Stack color="rgb(178,178,178)" gap={{base:"30px",md:"0px"}} direction={{base:"column",md:"row"}} justifyContent="center" alignItems={"center"} justifySelf={"center"} m="auto" maxW="80%">
+        <Stack direction={{base:"column",md:"column"}}>
+          <Heading fontSize={{base:"35px",md:"55px"}}>Watch everywhere.
 
 </Heading>
-          <Text fontSize={"30px"}>
+          <Text fontSize={{base:"20px",md:"30px"}}>
           Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.
 
 
@@ -132,13 +133,13 @@ export const Home = () => {
 
       <Container  borderBottom="8px"
         borderBottomColor={"rgb(34,34,34)"} p="40px" minW="100%" justifyContent={"center"} justifyItems="center" bgColor="rgb(0,0,0)">
-      <Stack color="rgb(178,178,178)" justifyContent={"space-between"} direction="row-reverse" alignItems={"center"} justifySelf={"center"} m="auto" maxW="80%">
-        <Stack direction={"column"}>
-          <Heading fontSize="55px">Create profiles for children.
+      <Stack color="rgb(178,178,178)"  gap={{base:"30px",md:"unset"}} justifyContent={"space-between"} direction={{base:"column",md:"row-reverse"}} alignItems={"center"} justifySelf={"center"} m="auto" maxW="80%">
+        <Stack direction={{base:"column",md:"column"}}>
+          <Heading fontSize={{base:"35px",md:"55px"}}>Create profiles for children.
 
 
 </Heading>
-          <Text fontSize={"30px"}>
+          <Text fontSize={{base:"20px",md:"30px"}}>
           Send children on adventures with their favourite characters in a space made just for them—free with your membership.
 
 
@@ -152,9 +153,9 @@ export const Home = () => {
      <Container  borderBottom="8px"
         borderBottomColor={"rgb(34,34,34)"} minW="100%" bgColor="rgb(0,0,0)">
 
-     <Container pb="40px" gap="20px" spacing="10px" color="rgb(255,255,255)" minW="65%" bgColor="rgb(0,0,0)">
-      <Stack direction="column" gap="10px">
-      <Heading fontSize="55px" pb="40px">
+     <Container pb={{base:"20px",md:"40px"}} gap="20px" spacing="10px" color="rgb(255,255,255)" minW="65%" bgColor="rgb(0,0,0)">
+      <Stack direction="column" textAlign={"center"} gap="10px">
+      <Heading fontSize={{base:"25px",md:"50px" }} pt="10px" pb="10px">
         Frequently Asked Questions
 
         </Heading>
@@ -164,7 +165,7 @@ export const Home = () => {
 <AccordionItem  bgColor="rgb(48,48,48)" border="0px">
     <h2>
       <AccordionButton height="60px">
-        <Box fontSize={"29px"} as="span" flex='1' textAlign='left'>
+        <Box fontSize={{base:"20px",md:"25px"}} as="span" flex='1' textAlign='left'>
           What is Netflix?
         </Box>
         <AccordionIcon />
@@ -180,7 +181,7 @@ You can watch as much as you want, whenever you want, without a single ad – al
   <AccordionItem  bgColor="rgb(48,48,48)" border="0px">
     <h2>
       <AccordionButton height="60px">
-        <Box fontSize={"29px"} as="span" flex='1' textAlign='left'>
+        <Box fontSize={{base:"20px",md:"25px"}} as="span" flex='1' textAlign='left'>
          How much does Netflix cost?
         </Box>
         <AccordionIcon />
@@ -195,7 +196,7 @@ You can watch as much as you want, whenever you want, without a single ad – al
   <AccordionItem  bgColor="rgb(48,48,48)" border="0px">
     <h2>
       <AccordionButton height="60px">
-        <Box fontSize={"29px"} as="span" flex='1' textAlign='left'>
+        <Box fontSize={{base:"20px",md:"25px"}} as="span" flex='1' textAlign='left'>
         Where can I watch?
         </Box>
         <AccordionIcon />
@@ -211,13 +212,13 @@ You can also download your favourite shows with the iOS, Android, or Windows 10 
   <AccordionItem  bgColor="rgb(48,48,48)" border="0px">
     <h2>
       <AccordionButton height="60px">
-        <Box fontSize={"29px"} as="span" flex='1' textAlign='left'>
+        <Box fontSize={{base:"20px",md:"25px"}} as="span" flex='1' textAlign='left'>
         How do I cancel?
         </Box>
         <AccordionIcon />
       </AccordionButton>
     </h2>
-    <AccordionPanel pb={4} fontSize="20px">
+    <AccordionPanel pb={4} fontSize={{base:"17px",md:"20px"}}>
     Netflix is flexible. There are no annoying contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.
 
     </AccordionPanel>
@@ -226,7 +227,7 @@ You can also download your favourite shows with the iOS, Android, or Windows 10 
     <AccordionItem  bgColor="rgb(48,48,48)" border="0px">
     <h2>
       <AccordionButton height="60px">
-        <Box fontSize={"29px"} as="span" flex='1' textAlign='left'>
+        <Box fontSize={{base:"20px",md:"25px"}} as="span" flex='1' textAlign='left'>
         What can I watch on Netflix?
         </Box>
         <AccordionIcon />
@@ -243,7 +244,7 @@ You can also download your favourite shows with the iOS, Android, or Windows 10 
 <AccordionItem  bgColor="rgb(48,48,48)" border="0px">
 <h2>
   <AccordionButton height="60px">
-    <Box fontSize={"29px"} as="span" flex='1' textAlign='left'>
+    <Box fontSize={{base:"20px",md:"25px"}}  as="span" flex='1' textAlign='left'>
 Is Netflix good for Kids?    </Box>
     <AccordionIcon />
   </AccordionButton>
